25a8a4b0962c8c6917167a04939614e7fa108c35
Ondrej Sika <ondrej@ondrejsika.com>
2025-09-16 08:47:27 +0200

    feat(examples/terraform/lxc): Update template, IP and root password

--- examples/terraform/lxc/main.tf
+++ examples/terraform/lxc/main.tf
@@ -22,8 +22,8 @@ resource "proxmox_lxc" "example" {
   start           = true
   target_node     = "pve0node0"
   hostname        = "lxc-${count.index}"
-  ostemplate      = "nfs:vztmpl/debian-12-standard_12.2-1_amd64.tar.zst"
-  password        = "asdfasdf"
+  ostemplate      = "nfs:vztmpl/debian-13-standard_13.1-1_amd64.tar.zst"
+  password        = "asdfasdf2020"
   ssh_public_keys = <<-EOT
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCslNKgLyoOrGDerz9pA4a4Mc+EquVzX52AkJZz+ecFCYZ4XQjcg2BK1P9xYfWzzl33fHow6pV/C6QC3Fgjw7txUeH7iQ5FjRVIlxiltfYJH4RvvtXcjqjk8uVDhEcw7bINVKVIS856Qn9jPwnHIhJtRJe9emE7YsJRmNSOtggYk/MaV2Ayx+9mcYnA/9SBy45FPHjMlxntoOkKqBThWE7Tjym44UNf44G8fd+kmNYzGw9T5IKpH1E1wMR+32QJBobX6d7k39jJe8lgHdsUYMbeJOFPKgbWlnx9VbkZh+seMSjhroTgniHjUl8wBFgw0YnhJ/90MgJJL4BToxu9PVnH ondrejsika
   EOT
@@ -38,7 +38,7 @@ ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCslNKgLyoOrGDerz9pA4a4Mc+EquVzX52AkJZz+ecF
   network {
     name   = "eth0"
     bridge = "vmbr1"
-    ip     = "10.255.0.${200 + count.index}/24"
-    gw     = "10.255.0.1"
+    ip     = "10.10.10.${200 + count.index}/24"
+    gw     = "10.10.10.1"
   }
 }
